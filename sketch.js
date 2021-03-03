de0bdcd487fc293ea1f812e6715ca944152fbc83
jaymesdec <jaymesdec@gmail.com>
2021-03-03 12:53:27 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var angle = 0;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  background(220);
+  background(220, 50);
   translate(windowWidth/2, windowHeight/2);
   rotate(angle);
-  line(0, 100, 0, -100);
-  ellipse(0, 100, 14, 14);
-  ellipse(0, -100, 14, 14);
+  fill(mouseX/2, mouseY/2, frameCount)
+  line(0, mouseY, 0, -mouseY);
+  ellipse(0, mouseY, 14, 14);
+  ellipse(0, -mouseY, 14, 14);
   angle += 0.1;
 }
